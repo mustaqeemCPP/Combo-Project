bb00ccf3e5ba9d1e7bd133c0ea029110f3251fee
Toheeb Obayemi <toheebobayemi@gmail.com>
2024-04-16 12:46:57 -0500

    added media query for mobile

--- script.js
+++ script.js
@@ -1,13 +1,2 @@
-// function openNav() {
-//     document.getElementById("myNavbar").style.backgroundColor = "red";
-//     // or you can use display block
-//     document.getElementById("myHeropage").style.marginTop = "400px";
-//     // or you can allow it to overlay on the hero section
-// }
 
-// function closeNav() {
-//     document.getElementById("myNavbar").style.width = "0";
-//     // or you can use display none
-//     document.getElementById("myHeropage").style.marginBottom = "0";
-// }
 

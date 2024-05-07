eb4a97c40e4f4626868ebfa2a71f0e9f83cf00fc
Toheeb Obayemi <toheebobayemi@gmail.com>
2024-05-07 11:34:35 -0500

    Changes - Mobile Nav Bar, Responsiveness & Image carousel-slider

--- script.js
+++ script.js
@@ -1,41 +1,142 @@
 
-// Hamburger Menu type 1
+// Hamburger Menu for mobile & Tablet view
 
-document.querySelector('.hamburger-button').addEventListener('click', sideBar);
+// Variable declaration
+const hamButton = document.querySelector('.hamburger-button');
+let navDisp = document.querySelector('.nav-links-mb');
 
+// Adding event to the hamburger button
+hamButton.addEventListener('click', sideBar);
+
+// Function to toggle the side bar display
 function sideBar() {
-    document.querySelector('.nav-links-mb').style.display = 
-    (document.querySelector('.nav-links-mb').style.display == 'none') ? 'flex' : 'none';
- };
+    if (navDisp.style.display == 'flex') {
+        navDisp.style.display = 'none';
+    } else {
+        navDisp.style.display = 'flex';
+    }
+}
+// End of Hamburger menu
 
+// Media query for image slider in review section
 
-//  Other ways to script the hamburger menu
+let sliderWidth1 = window.matchMedia('(min-width: 1012px)');
+let sliderWidth2 = window.matchMedia('(min-width: 510px) and (max-width: 768px)');
+let sliderWidth3 = window.matchMedia('(max-width: 500px)');
 
-// function openNav() {
-//     document.getElementById("myNavbar").style.backgroundColor = "red";
-//     // or you can use display block
-//     document.getElementById("myHeropage").style.marginTop = "400px";
-//     // or you can allow it to overlay on the hero section
-// }
+// Attach a change listener to confirm the viewport width
+sliderWidth1.addEventListener('change', sliderVp1);
+sliderWidth2.addEventListener('change', sliderVp1);
+sliderWidth3.addEventListener('change', sliderVp1);
 
-// function closeNav() {
-//     document.getElementById("myNavbar").style.width = "0";
-//     // or you can use display none
-//     document.getElementById("myHeropage").style.marginBottom = "0";
-// }
+// Variable declaration for the buttons on image slider
+const imageSlider = document.querySelector('.slider-container');
+const nxtBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.previous')
+const numberOfImages = document.querySelectorAll('.slider-container .sect6-col').length -1;
+let imageIndex = 1;
+let changeView = 0;
 
-//  function Nav() {
-//     if (display = "none") {
-//         display = "flex"
-//     }
-//     else {
-//         display = "none"
-//     }
-//  }
+// Call the function containing event listeners at run time
+sliderVp1();
 
-// function() {
-//     document.querySelector('.nav-links-mb').style.display = 
-//     (document.querySelector('.nav-links-mb').style.display == 'none') ? 'flex' : 'none';
-//  }
+// Function to slide the images based on viewport size
+function sliderVp1() {
+    
+    if (sliderWidth1.matches) {
+        nxtBtn.addEventListener('click', nxtImage);
+        prevBtn.addEventListener('click', prvImage);
 
+    } else if (sliderWidth2.matches) {
+        nxtBtn.addEventListener('click', nxtImage2);
+        prevBtn.addEventListener('click', prvImage2);
 
+    } else if (sliderWidth3.matches) {
+        nxtBtn.addEventListener('click', nxtImage3);
+        prevBtn.addEventListener('click', prvImage3);
+       }
+};
+
+// Image slider buttons for Desktop & Laptop
+
+// function for next button
+function nxtImage () {
+    if (imageIndex !== numberOfImages) {
+        changeView -= 400;
+        imageIndex ++;
+    } else {
+        return;
+    };
+
+    imageSlider.style.transform = `translateX(${changeView}px)`;
+};
+
+// function for previous button
+function prvImage () {
+    if (imageIndex !== 1) {
+        changeView += 400;
+        imageIndex --;
+
+    } else {
+        return;
+    }
+    imageSlider.style.transform = `translateX(${changeView}px)`;
+
+}
+
+// Image slider buttons for Tablet view
+
+// function for next button
+function nxtImage2 () {
+    if (imageIndex !== numberOfImages) {
+        changeView -= 500;
+        imageIndex ++;
+    } else {
+        return;
+    };
+
+    imageSlider.style.transform = `translateX(${changeView}px)`;
+
+};
+
+//function for previous button
+function prvImage2 () {
+    if (imageIndex !== 1) {
+        changeView += 500;
+        imageIndex --;
+    } else {
+        return;
+    }
+
+    imageSlider.style.transform = `translateX(${changeView}px)`;
+}
+
+// Image slider buttons for mobile view
+
+//function for next button
+function nxtImage3 () {
+    if (imageIndex !== 8) {
+        changeView -= 415;
+        imageIndex ++;
+        // alert(numberOfImages);
+    } else {
+        return;
+    };
+
+    imageSlider.style.transform = `translateX(${changeView}px)`;
+
+};
+
+//function for previous button
+function prvImage3 () {
+    if (imageIndex !== 1) {
+        changeView += 415;
+        imageIndex --;
+
+    } else {
+        return;
+    }
+
+    imageSlider.style.transform = `translateX(${changeView}px)`;
+
+}
